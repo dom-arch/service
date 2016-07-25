27793e7c2b884e732c41bcdff6c2c500e93d8100
Lcf.vs <lcf.vs@hotmail.com>
2016-07-25 07:08:49 +0200

    Update Url.php

--- classes/Lib/Url/Url.php
+++ classes/Lib/Url/Url.php
@@ -1,13 +1,33 @@
 <?php
 namespace Lib;
 
+use Doctrine\ORM\QueryBuilder;
+use DOMArch\Set;
+
 class Url extends \DOMArch\Url
 {
+    protected $_metas;
+
+    public function __construct(array $fields)
+    {
+        $this->_metas = Set::empty();
+
+        parent::__construct($fields);
+    }
+
+    /**
+     * @return Set
+     */
+    public function getMetas()
+    {
+        return $this->_metas;
+    }
+
     public function setModuleName(
         string $name
     )
     {
-        $this->getParams()
+        $this->getMetas()
             ->set('moduleName', $name);
 
         return $this;
@@ -15,7 +35,7 @@ class Url extends \DOMArch\Url
 
     public function getModuleName()
     {
-        return $this->getParams()
+        return $this->getMetas()
             ->get('moduleName');
     }
 
@@ -23,7 +43,7 @@ class Url extends \DOMArch\Url
         string $name
     )
     {
-        $this->getParams()
+        $this->getMetas()
             ->set('className', $name);
 
         return $this;
@@ -31,7 +51,7 @@ class Url extends \DOMArch\Url
 
     public function getClassName()
     {
-        return $this->getParams()
+        return $this->getMetas()
             ->get('className');
     }
     
@@ -39,7 +59,7 @@ class Url extends \DOMArch\Url
         int $id
     )
     {
-        $this->getParams()
+        $this->getMetas()
             ->set('entityId', $id);
 
         return $this;
@@ -47,7 +67,7 @@ class Url extends \DOMArch\Url
 
     public function getEntityId()
     {
-        return $this->getParams()
+        return $this->getMetas()
             ->get('entityId');
     }
 
@@ -55,7 +75,7 @@ class Url extends \DOMArch\Url
         int $id
     )
     {
-        $this->getParams()
+        $this->getMetas()
             ->set('subEntityId', $id);
 
         return $this;
@@ -63,7 +83,117 @@ class Url extends \DOMArch\Url
 
     public function getSubEntityId()
     {
-        return $this->getParams()
+        return $this->getMetas()
             ->get('subEntityId');
+    }
+
+    public function buildQuery(
+        QueryBuilder $builder
+    )
+    {
+        $alias = $builder->getRootAliases()[0] . '.';
+        
+        foreach ($this->getParams()->toArray() as $name => $value) {
+            if ($name[0] !== '$') {
+                $name = $alias . $name;
+                $builder->andWhere($builder->expr()->eq($name, $value));
+
+                continue;
+            }
+
+            if ($name === '$limit') {
+                $builder->setMaxResults($value);
+
+                continue;
+            }
+
+            if ($name === '$offset') {
+                $builder->setFirstResult($value);
+
+                continue;
+            }
+
+            if ($name === '$order') {
+                foreach ($value as $values) {
+                    $values[0] = $alias . $values[0];
+                    
+                    $builder->orderBy(...$values);
+                }
+
+                continue;
+            }
+
+            if ($name === '$gt') {
+                foreach ($value as $values) {
+                    $values[0] = $alias . $values[0];
+
+                    $builder->expr()->gt(...$values);
+                }
+
+                continue;
+            }
+
+            if ($name === '$gte') {
+                foreach ($value as $values) {
+                    $values[0] = $alias . $values[0];
+
+                    $builder->expr()->gte(...$values);
+                }
+
+                continue;
+            }
+
+            if ($name === '$lt') {
+                foreach ($value as $values) {
+                    $values[0] = $alias . $values[0];
+
+                    $builder->expr()->lt(...$values);
+                }
+
+                continue;
+            }
+
+            if ($name === '$lte') {
+                foreach ($value as $values) {
+                    $values[0] = $alias . $values[0];
+
+                    $builder->expr()->lte(...$values);
+                }
+
+                continue;
+            }
+
+            if ($name === '$null') {
+                foreach ($value as $values) {
+                    $values[0] = $alias . $values[0];
+
+                    $builder->expr()->isNull(...$values);
+                }
+
+                continue;
+            }
+
+            if ($name === '$like') {
+                foreach ($value as $values) {
+                    $values[0] = $alias . $values[0];
+
+                    $builder->expr()->like(...$values);
+                }
+
+                continue;
+            }
+
+            if ($name === '$in') {
+                foreach ($value as $values) {
+                    $values[0] = $alias . $values[0];
+
+                    $builder->expr()->in(...$values);
+                }
+
+                continue;
+            }
+        }
+
+        return $builder;
     }
 }
